e8976cffb1f90cf7a293f80471299c14068e7128
Tom Yu <kangleyu@gmail.com>
2017-03-29 22:10:54 +0800

    move component html to external file and add gulp copy task for copying html files to views folder

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,13 @@ gulp.task('build', function() {
   return tsResult.js.pipe(gulp.dest('./built'));
 });
 
+// task for copying all *.html files to views folder
+var sourceFiels = ['src/**/*.html'];
+var destination = 'views/';
+gulp.task('copy', function() {
+  return gulp.src(sourceFiels).pipe(gulp.dest(destination));
+})
+
 // watch task for monitoring any .ts file changes
 gulp.task('watch', ['build'], function() {
   gulp.watch('./**/*.ts', ['build']);
--- src/pomodoro-timer.ts
+++ src/pomodoro-timer.ts
@@ -5,23 +5,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pomodoro-timer',
-  template: `
-  <div class="container text-center">
-    <img src="assets/img/pomodoro.png" style="width:116px;height:86px" />
-    <countdown [seconds]="25"
-      (complete)="onCountdownCompleted()"
-      #counter>
-    </countdown>
-    <p>
-      <button class="btn btn-default" (click)="counter.seconds = 25">
-        Reset countdown to 25 seconds
-      </button>
-    </p>
-    <p *ngIf="counter.seconds < 10">
-      Beware! Only <strong>{{counter.seconds}} seconds</strong> left.
-    </p>
-  </div>
-  `
+  templateUrl: './views/pomodoro-timer.html'
 })
 export class PomodoroTimerComponent {
   // timeout: number; /** no longer required */
