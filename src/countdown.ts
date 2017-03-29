f98ffadd8e0c4574bfec02f254fd7b0e44fe77ad
Tom Yu <kangleyu@gmail.com>
2017-03-29 21:53:22 +0800

    show an alternative way for setting inputs and outputs of component

--- src/countdown.ts
+++ src/countdown.ts
@@ -2,7 +2,11 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'countdown',
-  template: '<h1>Time left: {{seconds}}</h1>'
+  template: '<h1>Time left: {{seconds}}</h1>',
+  // an alternative way for setting inputs and outputs
+  // then we can remove @Input() and @Output() decorators
+  // inputs: ['seconds'],
+  // outputs: ['complete', 'progress']
 })
 export class CountdownComponent {
   // Set input field for the Component
