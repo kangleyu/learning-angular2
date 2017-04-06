734299d6fc7f2dfdc2e1d626c9f300ce107f9092
Tom Yu <kangleyu@gmail.com>
2017-04-06 21:34:24 +0800

    Add title for the task-editor component

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule, Title  } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import AppComponent from './app.component';
 import { FormattedTimePipe, QueuedOnlyPipe, CanActivateService } from './shared/shared';
 import { TimerWidgetComponent } from './timer/timer';
 import { TasksComponent, TaskIconsComponent, TaskTooltipDirective, TaskEditorComponent } from './tasks/tasks';
 
+
 const appRoutes: Routes = [
   { path: '', component: TasksComponent },
   { 
     path: 'tasks/editor', 
     canActivate: [ CanActivateService ],
-    component: TaskEditorComponent 
+    component: TaskEditorComponent,
+    data: { title: 'Welcome to the Task Form!' }
   },
   { 
     path: 'timer', 
@@ -49,6 +51,7 @@ const appRoutes: Routes = [
     TaskEditorComponent
   ],
   providers: [
+    Title,
     CanActivateService
   ],
   bootstrap: [AppComponent],
--- src/tasks/task-editor.component.ts
+++ src/tasks/task-editor.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'pomodoro-tasks-editor',
   templateUrl:'public/tasks/task-editor.component.html'
 })
 export default class TaskEditorComponent {
-  constructor() {}
+  constructor(private titleService: Title, private router: Router) {
+    router.events.subscribe(event => {
+      if(event instanceof NavigationEnd) {
+        var title = this.getTitle(router.routerState, router.routerState.root).join('-');
+        titleService.setTitle(title);
+      }
+    });
+  }
+
+  private getTitle(state, parent) {
+    var data = [];
+    if(parent && parent.snapshot.data && parent.snapshot.data.title) {
+      data.push(parent.snapshot.data.title);
+    }
+
+    if(state && parent) {
+      data.push(... this.getTitle(state, state.firstChild(parent)));
+    }
+    return data;
+  }
 }
